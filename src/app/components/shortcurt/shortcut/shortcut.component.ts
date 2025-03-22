a75996798e52de329da184ace9585c7d731ba327
Fedor Nikolaev <f.nikolaev@elma-bs.ru>
2025-03-22 19:33:36 +0400

    feat: validation

--- src/app/components/shortcurt/shortcut/shortcut.component.ts
+++ src/app/components/shortcurt/shortcut/shortcut.component.ts
@@ -7,26 +7,29 @@ import { distinctUntilChanged, filter, fromEvent, map, merge, share, Subject, ta
   styleUrl: './shortcut.component.scss'
 })
 export class ShortcutComponent implements OnInit {
-    @Input() modifiers: string[] = ['Control', 'Alt', 'Shift', 'CapsLock', 'Meta'];
+    @Input({ required: true }) modifiers!: string[];
 
     @ViewChild('shortcutInput', { static: true }) private shortcutInput!: ElementRef;
 
     protected currentPressedKeys: string[] = [];
+    private countOfModifiers = 0;
+    private countOfNotModifiers = 0;
+    protected resultKeys$ = new Subject<string[]>();
 
     ngOnInit(): void {
         const keydown$ = fromEvent<KeyboardEvent>(this.shortcutInput.nativeElement, 'keydown');
-        const keyup$ = (fromEvent<KeyboardEvent>(this.shortcutInput.nativeElement, 'keyup')).pipe(share());
+        const keyup$ = fromEvent<KeyboardEvent>(this.shortcutInput.nativeElement, 'keyup');
 
         merge(
             keydown$.pipe(
                 tap((event) => {
                     event.preventDefault();
-                    event.stopPropagation();
                 }),
                 map((event) => {
                     return event.key;
                 }),
             ),
+            // to reset the distinctUntilChanged state
             keyup$.pipe(
                 map(() => '')
             ),
@@ -35,17 +38,51 @@ export class ShortcutComponent implements OnInit {
                 distinctUntilChanged(),
                 filter((key) => !!key),
                 tap((key) => {
-                    this.currentPressedKeys.push(key)
-                })
+                    this.updateCurrentKeysCountsKeydown(key);
+                    this.currentPressedKeys.push(key);
+                }),
             )
             .subscribe(() => {
-                console.log(this.currentPressedKeys);
+                this.updateResultKeysValue();
             })
 
         keyup$.subscribe((event: KeyboardEvent) => {
-            console.log(event);
-            this.currentPressedKeys = this.currentPressedKeys.filter((key) => key !== event.key);
-            console.log(this.currentPressedKeys);
+            const keyIndex = this.currentPressedKeys.findIndex((key) => key === event.key);
+            if (keyIndex < 0) {
+                return;
+            }
+
+            this.updateCurrentKeysCountsKeyup(event.key);
+            this.currentPressedKeys.splice(keyIndex, 1)
+            this.updateResultKeysValue();
         })
+    }
+
+    private updateCurrentKeysCountsKeydown(key: string) {
+        if(this.modifiers.includes(key)) {
+            this.countOfModifiers++;
+        } else {
+            this.countOfNotModifiers++;
+        }
+    }
+
+    private updateCurrentKeysCountsKeyup(key: string) {
+        if(this.modifiers.includes(key)) {
+            this.countOfModifiers--;
+        } else {
+            this.countOfNotModifiers--;
+        }
+    }
+
+    private updateResultKeysValue() {
+        console.log(this.countOfModifiers, this.countOfNotModifiers);
+        if(
+            this.countOfModifiers > 0 &&
+            this.countOfNotModifiers === 1
+        ) {
+            this.resultKeys$.next(this.currentPressedKeys)
+        } else {
+            this.resultKeys$.next([])
+        }
     }
 }
