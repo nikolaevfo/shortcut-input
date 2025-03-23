31903e8413aad162d0c15a3c88eeb93b5748188e
Fedor Nikolaev <f.nikolaev@elma-bs.ru>
2025-03-23 06:55:31 +0400

    fix: validation

--- src/app/components/shortcurt/shortcut/shortcut.component.ts
+++ src/app/components/shortcurt/shortcut/shortcut.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
-import { distinctUntilChanged, filter, fromEvent, map, merge, share, Subject, tap } from 'rxjs';
+import { BehaviorSubject, distinctUntilChanged, filter, fromEvent, map, merge, share, Subject, tap } from 'rxjs';
 
 @Component({
   selector: 'app-shortcut',
@@ -12,26 +12,26 @@ export class ShortcutComponent implements OnInit {
     @ViewChild('shortcutInput', { static: true }) private shortcutInput!: ElementRef;
 
     protected currentPressedKeys: string[] = [];
-    private countOfModifiers = 0;
-    private countOfNotModifiers = 0;
-    protected resultKeys$ = new Subject<string[]>();
+    private countOfModifiersPressedKeys = 0;
+    private countOfNotModifiersPressedKeys = 0;
 
-    ngOnInit(): void {
+    protected showedKeys$ = new BehaviorSubject<string[]>([]);
+    protected inProgress = false;
+    protected isValid = false;
+    protected currentValidStateExist = false;
+
+    ngOnInit (): void {
         const keydown$ = fromEvent<KeyboardEvent>(this.shortcutInput.nativeElement, 'keydown');
         const keyup$ = fromEvent<KeyboardEvent>(this.shortcutInput.nativeElement, 'keyup');
 
         merge(
             keydown$.pipe(
-                tap((event) => {
-                    event.preventDefault();
-                }),
-                map((event) => {
-                    return event.key;
-                }),
+                tap((event) => event.preventDefault()),
+                map((event) => event.key),
             ),
             // to reset the distinctUntilChanged state
             keyup$.pipe(
-                map(() => '')
+                map(() => ''),
             ),
         )
             .pipe(
@@ -40,49 +40,92 @@ export class ShortcutComponent implements OnInit {
                 tap((key) => {
                     this.updateCurrentKeysCountsKeydown(key);
                     this.currentPressedKeys.push(key);
+                    this.inProgress = true;
                 }),
             )
-            .subscribe(() => {
-                this.updateResultKeysValue();
+            .subscribe((key) => {
+                this.onKeyDownHandler(key);
             })
 
-        keyup$.subscribe((event: KeyboardEvent) => {
-            const keyIndex = this.currentPressedKeys.findIndex((key) => key === event.key);
-            if (keyIndex < 0) {
-                return;
+        keyup$
+            .pipe(
+                map((event) => event.key),
+                tap((key) => {
+                    const keyIndex = this.currentPressedKeys.findIndex((pressedKey) => pressedKey === key);
+                    if (keyIndex < 0) {
+                        return;
+                    }
+
+                    this.updateCurrentKeysCountsKeyup(key);
+                    this.currentPressedKeys.splice(keyIndex, 1);
+
+                    if(this.currentPressedKeys.length === 0) {
+                        this.inProgress = false;
+                    }
+                })
+            )
+            .subscribe((key: string) => {
+                this.onKeyUpHandler(key);
+            })
+    }
+
+    private onKeyDownHandler (key: string) {
+        // if the component has a valid state before
+        if (this.currentValidStateExist) {
+            // render the old valid state until we get a new valid state
+            if (this.inputValidity) {
+                this.isValid = true;
+
+                this.showedKeys$.next([...this.currentPressedKeys]);
+            } else {
+                this.isValid = false;
             }
 
-            this.updateCurrentKeysCountsKeyup(event.key);
-            this.currentPressedKeys.splice(keyIndex, 1)
-            this.updateResultKeysValue();
-        })
-    }
+            return;
+        }
 
-    private updateCurrentKeysCountsKeydown(key: string) {
-        if(this.modifiers.includes(key)) {
-            this.countOfModifiers++;
+        // If the component doesn't have a valid state before,
+        // render the pressed key inside the shortcut-input
+        this.showedKeys$.next([...this.currentPressedKeys]);
+
+        // update currentValidState if valid
+        if (this.inputValidity) {
+            this.currentValidStateExist = true;
+            this.isValid = true;
+
+            this.showedKeys$.next([...this.currentPressedKeys]);
         } else {
-            this.countOfNotModifiers++;
+            this.isValid = false;
         }
     }
 
-    private updateCurrentKeysCountsKeyup(key: string) {
-        if(this.modifiers.includes(key)) {
-            this.countOfModifiers--;
-        } else {
-            this.countOfNotModifiers--;
+    private onKeyUpHandler (key: string) {
+        // if the component doesn't have a valid state before and the current value is invalid
+        if (!this.currentValidStateExist && !this.inputValidity) {
+            // remove it from the render
+            this.showedKeys$.next([]);
+            return;
         }
     }
 
-    private updateResultKeysValue() {
-        console.log(this.countOfModifiers, this.countOfNotModifiers);
-        if(
-            this.countOfModifiers > 0 &&
-            this.countOfNotModifiers === 1
-        ) {
-            this.resultKeys$.next(this.currentPressedKeys)
+    private updateCurrentKeysCountsKeydown (key: string) {
+        if (this.modifiers.includes(key)) {
+            this.countOfModifiersPressedKeys++;
         } else {
-            this.resultKeys$.next([])
+            this.countOfNotModifiersPressedKeys++;
         }
+    }
+
+    private updateCurrentKeysCountsKeyup (key: string) {
+        if (this.modifiers.includes(key)) {
+            this.countOfModifiersPressedKeys--;
+        } else {
+            this.countOfNotModifiersPressedKeys--;
+        }
+    }
+
+    private get inputValidity (): boolean {
+        return this.countOfModifiersPressedKeys > 0 &&
+            this.countOfNotModifiersPressedKeys === 1;
     }
 }
